437925627bf7d877914f1c4f1ccf52b3ec19e302
Ronashovkova <rona_t@icloud.com>
2020-02-22 23:21:38 +0200

    reformated code

--- src/com/cursor/models/WebServer.java
+++ src/com/cursor/models/WebServer.java
@@ -1,14 +1,14 @@
 package com.cursor.models;
 
-    public class WebServer implements Runnable{
-        private Request request;
+public class WebServer implements Runnable {
+    private Request request;
 
-        public WebServer(Request request) {
-            this.request = request;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("This thread has request " + request);
-        }
+    public WebServer(Request request) {
+        this.request = request;
     }
+
+    @Override
+    public void run() {
+        System.out.println("This thread has request " + request);
+    }
+}
